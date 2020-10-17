b33aa88a934fcee74f23ba5361567b0e406855ae
tom_glb <goodMorning_glb@atguigu.com>
2020-10-17 21:06:04 +0800

    支分支

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello 更新");
         System.out.println("创建分支");
+        System.out.println("支分支");
     }
 }
